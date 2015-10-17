fb2d633d00612545e829bae41c8162fefe4af171
Mark Staebler <markstaebler@gmail.com>
2015-10-16 23:20:58 -0700

    added a history display

--- js/index.js
+++ js/index.js
@@ -7,10 +7,12 @@ $(document).ready(function() {
   var myViewModel = function(){
     var self = this, operator = '', num1 = '', num2 = '', sum = 0, firstNum = true;
     self.buttons = ko.observableArray(buttonArray);
-    self.result = ko.observable(0);
+    self.result  = ko.observable(0);
+    self.historyDisp = ko.observable('');
     self.keyPress = function(key) {
           if(key === 'CE'){
             self.result(0);
+            self.historyDisp('');
             num1 = '';
             num2 = '';
             operator = '';
@@ -21,54 +23,65 @@ $(document).ready(function() {
             operator = key;
             firstNum = false;
             self.result(key);
+            self.historyDisp(historyDisp()+key);
             }else{
               if(key === '%'){
               sum = parseFloat(num1)%parseFloat(num2);
               self.result(sum);
+              self.historyDisp(historyDisp()+key);
               num1 = sum;
-              console.log(num1+''+sum);
+              
             }else if(key === 'x'){
               sum = parseFloat(num1)*parseFloat(num2);
               self.result(sum);
+              self.historyDisp(historyDisp()+key);
               num1 = sum;
-              console.log(num1+''+sum);
+              
             }else if(key === '/'){
               sum = parseFloat(num1)/parseFloat(num2);
               self.result(sum);
+              self.historyDisp(historyDisp()+key);
               num1 = sum;
-              console.log(num1+''+sum);
+              
             }else if(key === '+'){
               sum = parseFloat(num1)+parseFloat(num2);
               self.result(sum);
+              self.historyDisp(historyDisp()+key);
               num1 = sum;
-              console.log(num1+''+sum);
+              
             }else {
               sum = parseFloat(num1)-parseFloat(num2);
               self.result(sum);
+              self.historyDisp(historyDisp()+key);
               num1 = sum;
-              console.log(num1+''+sum);
+              
             }
             }
           }else if(key === "="){
             if(operator === '%'){
               sum = parseFloat(num1)%parseFloat(num2);
               self.result(sum);
+              
               num1 = sum;
             }else if(operator === 'x'){
               sum = parseFloat(num1)*parseFloat(num2);
               self.result(sum);
+              
               num1 = sum;
             }else if(operator === '/'){
               sum = parseFloat(num1)/parseFloat(num2);
               self.result(sum);
+              
               num1 = sum;
             }else if(operator === '+'){
               sum = parseFloat(num1)+parseFloat(num2);
               self.result(sum);
+              
               num1 = sum;
             }else {
               sum = parseFloat(num1)-parseFloat(num2);
               self.result(sum);
+              
               num1 = sum;
             }
             num1 = '';
@@ -80,14 +93,17 @@ $(document).ready(function() {
           }else if(firstNum){
             num1 = num1 + key;
             self.result(num1);
+            self.historyDisp(historyDisp()+num1);
           }else{
             
             if(sum>0){
               num2 = key;
               self.result(sum);
+              self.historyDisp(historyDisp()+num2);
             }else{
             num2 = num2 + key;
             self.result(num2);
+            self.historyDisp(historyDisp()+num2);
             }
           }
     };
