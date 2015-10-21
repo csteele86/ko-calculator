6cf6534821a465b16e4e629a1fe12035a49c7ab2
Mark Staebler <markstaebler@gmail.com>
2015-10-20 21:27:56 -0700

    all user stories and bonus user stores implemented

--- js/index.js
+++ js/index.js
@@ -1,124 +1,32 @@
 $(document).ready(function() {
-  var buttonArray = ['CE', '%', 'x', '/', '+', '-', '.'];
+  var buttonArray = ['CE', '%', '*', '/', '+', '-', '.'];
   for (var i = 9; i >= 0; i--) {
     buttonArray.push(i.toString());
   }
   buttonArray.push("=");
 
   var myViewModel = function() {
-    var self = this,
-      operator = '',
-      num1 = '',
-      num2 = '',
-      sum = 0,
-      firstNum = true;
-    self.buttons = ko.observableArray(buttonArray);
-    self.result = ko.observable(0);
-    self.historyDisp = ko.observable('');
-    self.reset = function(){
-      self.result(0);
-      self.historyDisp('');
-      num1 = '';
-      num2 = '';
-      operator = '';
-      firstNum = true;
-      sum = 0;
+    var self = this; //set the value for this
+
+    self.buttons = ko.observableArray(buttonArray);//button array
+    self.result = ko.observable('0');//result box
+
+    self.reset = function(){ //reset function
+      self.result('0');
     }
     self.keyPress = function(key) {
       if (key === 'CE') {
-        self.reset();
-      } else if (key === '%' || key === 'x' || key === '/' || key === '+' || key === '-') {
-        if (firstNum) {
-          operator = key;
-          firstNum = false;
-          self.result(key);
-          self.historyDisp(historyDisp() + key);
-        } else {
-          if (key === '%') {
-            sum = parseFloat(num1) % parseFloat(num2);
-            self.result(sum);
-            self.historyDisp(historyDisp() + key);
-            num1 = sum;
-
-          } else if (key === 'x') {
-            sum = parseFloat(num1) * parseFloat(num2);
-            self.result(sum);
-            self.historyDisp(historyDisp() + key);
-            num1 = sum;
-
-          } else if (key === '/') {
-            sum = parseFloat(num1) / parseFloat(num2);
-            self.result(sum);
-            self.historyDisp(historyDisp() + key);
-            num1 = sum;
-
-          } else if (key === '+') {
-            sum = parseFloat(num1) + parseFloat(num2);
-            self.result(sum);
-            self.historyDisp(historyDisp() + key);
-            num1 = sum;
-
-          } else {
-            sum = parseFloat(num1) - parseFloat(num2);
-            self.result(sum);
-            self.historyDisp(historyDisp() + key);
-            num1 = sum;
-
-
-          }
+        self.reset();//reset if clear is pressed
+      } else if (key === '='){
+        self.result(eval(self.result()));//evaluate the string when equals is pressed
+      }else {
+        if(self.result()==='0'){
+          self.result('');//fix to show an initial zero but remove it at first number
         }
-      } else if (key === "=") {
-        if (operator === '%') {
-          sum = parseFloat(num1) % parseFloat(num2);
-          self.result(sum);
-
-          num1 = sum;
-        } else if (operator === 'x') {
-          sum = parseFloat(num1) * parseFloat(num2);
-          self.result(sum);
-
-          num1 = sum;
-        } else if (operator === '/') {
-          sum = parseFloat(num1) / parseFloat(num2);
-          self.result(sum);
-
-          num1 = sum;
-        } else if (operator === '+') {
-          sum = parseFloat(num1) + parseFloat(num2);
-          self.result(sum);
-
-          num1 = sum;
-        } else {
-          sum = parseFloat(num1) - parseFloat(num2);
-          self.result(sum);
-
-          num1 = sum;
-        }
-        num1 = '';
-        num2 = '';
-        operator = '';
-        sum = 0;
-        firstNum = true;
-
-      } else if (firstNum) {
-        num1 = num1 + key;
-        self.result(num1);
-        self.historyDisp(historyDisp() + num1);
-      } else {
-
-        if (sum > 0) {
-          num2 = key;
-          self.result(sum);
-          self.historyDisp(historyDisp() + key);
-        }else {
-          num2 = num2 + key;
-          self.result(num2);
-          self.historyDisp(historyDisp() + key);
-        }
+        self.result(self.result()+key);//append the next char to the string
       }
-    };
-  };
-
-  ko.applyBindings(myViewModel, document.getElementById("calculator"));
+  }
+}
+  ko.applyBindings(myViewModel, document.getElementById("calculator"));//apply bindings
 
 });
