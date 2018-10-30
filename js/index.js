0605ab24cd8c9bf859d724b43fcf1476aa57b5af
Chris Steele <csteele86@gmail.com>
2018-10-30 11:02:10 -0400

    Refactoring to prepare for tests

--- js/index.js
+++ js/index.js
@@ -1,32 +1,39 @@
 $(document).ready(function() {
+  init();
+});
+
+function init() {
+  ko.applyBindings(myViewModel, document.getElementById("calculator"));  
+}
+
+var myViewModel = function() {
+  var self = this; //set the value for this
+
+  self.buttons = ko.observableArray(getButtons());//button array
+  self.result = ko.observable('0');//result box
+
+  self.reset = function(){ //reset function
+    self.result('0');
+  }
+  self.keyPress = function(key) {
+    if (key === 'CE') {
+      self.reset();//reset if clear is pressed
+    } else if (key === '='){
+      self.result(eval(self.result()));//evaluate the string when equals is pressed
+    }else {
+      if(self.result()==='0'){
+        self.result('');//fix to show an initial zero but remove it at first number
+      }
+      self.result(self.result()+key);//append the next char to the string
+    }
+  }
+}
+
+function getButtons() {
   var buttonArray = ['CE', '%', '*', '/', '+', '-', '.'];
   for (var i = 9; i >= 0; i--) {
     buttonArray.push(i.toString());
   }
   buttonArray.push("=");
-
-  var myViewModel = function() {
-    var self = this; //set the value for this
-
-    self.buttons = ko.observableArray(buttonArray);//button array
-    self.result = ko.observable('0');//result box
-
-    self.reset = function(){ //reset function
-      self.result('0');
-    }
-    self.keyPress = function(key) {
-      if (key === 'CE') {
-        self.reset();//reset if clear is pressed
-      } else if (key === '='){
-        self.result(eval(self.result()));//evaluate the string when equals is pressed
-      }else {
-        if(self.result()==='0'){
-          self.result('');//fix to show an initial zero but remove it at first number
-        }
-        self.result(self.result()+key);//append the next char to the string
-      }
-  }
+  return buttonArray;
 }
-  ko.applyBindings(myViewModel, document.getElementById("calculator"));//apply bindings
-
-});
