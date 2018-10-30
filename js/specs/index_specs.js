3200c63cb831ea31e82f3a670687d67b8ba6d70e
Chris Steele <csteele86@gmail.com>
2018-10-30 15:03:58 -0400

    Refactoring to use setup and teardowns

--- js/specs/index_specs.js
+++ js/specs/index_specs.js
@@ -1,95 +1,100 @@
-describe("Index", function(){
-    //it is the test and must be within a describe
-    it('should get all of the buttons', () => {
+describe("Index", function() { 
+    //beforeAll will run once before all tests
+    //use this to help reduce expensive test setup
+    //recommended to have a corresponding afterAll to redo what was done here
+    beforeAll(function(){
+        console.log("Running once before all tests");
+    });
+
+    it('should get all of the buttons', function() {
         var buttons = getButtons();
 
-        //toBe uses === and checks for reference equality
         expect(buttons.length).toBe(18);        
 
-        //toContain will check arrays
         expect(buttons).toContain("=");
 
-        //using "not" negates the matcher
         expect(buttons).not.toContain("10");
     });
 
-    //adding an x will disable the test
-    xit('should apply the knockout bindings', () => {
+    xit('should apply the knockout bindings', function() {
         //we will revisit this later
     });
 
-    //describe can be nested, but not an it
-    describe('view model', () => {
-        it('should initialize a view model', () => {
-            var viewModel = new myViewModel();
-    
-            //toBeTruthy uses boolean casting testing
+    describe('view model', function() {
+        //beforeEach will run before each test within a describe
+        //helps you to write DRY (don't repeat yourself) code
+        beforeEach(function() {
+            //think of 'this' as the test context. 
+            //it is only availabe within a beforeEach and afterEach 
+            //note that 'this' cannot be passed to arrow functions (i.e., () => )
+            this.viewModel = new myViewModel();                
+        });
+
+        it('should initialize a view model', function() {
             expect(self.buttons()).toBeTruthy();
     
-            //toEqual uses === and checks for property equivalency (i.e., primitive types)
             expect(self.result()).toEqual('0');
         });
     
-        it('should reset the calculator', () => {
-            var viewModel = new myViewModel();
-    
-            viewModel.result('100');
-            viewModel.reset();
+        it('should reset the calculator', function() {
+            this.viewModel.result('100');
+            this.viewModel.reset();
             
-            expect(viewModel.result()).toBe('0');
+            expect(this.viewModel.result()).toBe('0');
         });
 
-        //another describe since still in the context of the view model
-        describe("when a key is pressed", function() {
-            it('the CE button should reset the calculator', () => {
-                var viewModel = new myViewModel();
-                
-                viewModel.result('100');
+        describe("when a key is pressed", function() {            
+            it('the CE button should reset the calculator', function() {
+                //take note that we are in a different describe and we are still accessing 'this'
+                this.viewModel.result('100');
     
-                viewModel.keyPress('CE');
+                this.viewModel.keyPress('CE');
     
-                expect(viewModel.result()).toBe('0');
+                this.expect = '0';
             });
 
-            it('the = button should calculate', () => {
-                var viewModel = new myViewModel();
-                
-                viewModel.result('10*10');
+            it('the = button should calculate', function() {
+                this.viewModel.result('10*10');
     
-                viewModel.keyPress('=');
+                this.viewModel.keyPress('=');
     
-                expect(viewModel.result()).toBe('100');
+                this.expect = 100;
             });
 
-            it('the 0 button should ensure the result is 0 instead of 00', () => {
-                var viewModel = new myViewModel();
-                
-                viewModel.result('0');
+            it('the 0 button should ensure the result is 0 instead of 00', function() {
+                this.viewModel.result('0');
     
-                viewModel.keyPress('0');
+                this.viewModel.keyPress('0');
     
-                expect(viewModel.result()).toBe('0');
+                this.expect = '0';
             });
 
-            it('any non-zero number button should add to the existing result', () => {
-                var viewModel = new myViewModel();
-                
-                viewModel.result('19');
+            it('any non-zero number button should add to the existing result', function() {
+                this.viewModel.result('19');
     
-                viewModel.keyPress('1');
+                this.viewModel.keyPress('1');
     
-                expect(viewModel.result()).toBe('191');
+                this.expect = '191';
             });
 
-            it('any non-number button that is not an = or CE should add to the existing result', () => {
-                var viewModel = new myViewModel();
-                
-                viewModel.result('19');
+            it('any non-number button that is not an = or CE should add to the existing result', function() {
+                this.viewModel.result('19');
     
-                viewModel.keyPress('*');
+                this.viewModel.keyPress('*');
     
-                expect(viewModel.result()).toBe('19*');
+                this.expect = '19*';
+            });
+
+            //aferEach will run after each test within a descirbe
+            afterEach(function() {
+                expect(this.viewModel.result()).toBe(this.expect);                
             });
         });        
-    });    
+    }); 
+    
+    //afterAll will run once after all tests
+    //use this to undo changes done in beforeAll
+    afterAll(function(){
+        console.log("Running once after all tests.");
+    });
 });
